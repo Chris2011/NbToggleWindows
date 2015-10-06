cf592df78500837d2df6f119dc4f049be6cc8028
Chris <chrl@spreadshirt.net>
2015-10-06 09:49:30 +0200

    First integration of the logic.

---
+++ src/org/chrisle/netbeans/plugins/nbtogglewindows/NbToggleWindowsAction.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.chrisle.netbeans.plugins.nbtogglewindows;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.function.Consumer;
+import javax.swing.JOptionPane;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.nodes.Sheet.Set;
+import org.openide.util.NbBundle.Messages;
+import org.openide.windows.Mode;
+import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
+
+@ActionID(
+        category = "Window",
+        id = "org.chrisle.netbeans.plugins.nbhidewindows.NbToggleWindowsAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_NbToggleWindowsAction"
+)
+@ActionReference(path = "Shortcuts", name = "DOS-ENTER")
+@Messages("CTL_NbToggleWindowsAction=Toggle Windows")
+public final class NbToggleWindowsAction implements ActionListener {
+    Collection<TopComponent> _displayedComps = new HashSet<>();
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Collection<TopComponent> comps = getDisplayedWindows();
+        
+        if (comps.size() > 0) {
+            comps.forEach(new Consumer<TopComponent>() {
+                @Override
+                public void accept(TopComponent t) {
+                    if (t.isDisplayable()) {
+                        t.close();
+                    } else {
+                        t.open();
+                    }
+                }
+            });
+        } else {
+            _displayedComps.forEach(new Consumer<TopComponent>() {
+                @Override
+                public void accept(TopComponent t) {
+                    if (t.isDisplayable()) {
+                        t.close();
+                    } else {
+                        t.open();
+                    }
+                }
+            });
+        }
+        
+//        Collection<TopComponent> comps = getDisplayedWindows();
+//        comps.forEach(new Consumer<TopComponent>() {
+//            @Override
+//            public void accept(TopComponent t) {
+//                if (t.isDisplayable()) {
+//                    _displayedComps.add(t);
+//
+//                    t.close();
+//
+////                    JOptionPane.showMessageDialog(null, t.getName(), "Opened Windows", JOptionPane.WARNING_MESSAGE);
+//                } else {
+//                    for (Iterator<TopComponent> iterator = _displayedComps.iterator(); iterator.hasNext();) {
+//                        TopComponent next = iterator.next();
+//
+//                        next.open();
+//                    }
+//                }
+//            }
+//        });
+    }
+
+    private Collection<TopComponent> getDisplayedWindows() {
+        final ArrayList<TopComponent> result = new ArrayList<TopComponent>();
+        final WindowManager wm = WindowManager.getDefault();
+
+        for (Mode mode : wm.getModes()) {
+            if (!wm.isEditorMode(mode)) {
+                Arrays.asList(wm.getOpenedTopComponents(mode)).forEach(new Consumer<TopComponent>() {
+                    @Override
+                    public void accept(TopComponent t) {
+                        if (t.isDisplayable() && !_displayedComps.contains(t)) {
+                            _displayedComps.add(t);
+
+                            result.add(t);
+                        }
+                    }
+                });
+                
+            }
+        }
+
+        return result;
+    }
+}
